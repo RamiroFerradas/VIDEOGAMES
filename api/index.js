4840234e246a71a818d8054ea65b956a6de8b2e0
Ramiro <ramiroferradas@hotmail.com>
2022-08-20 19:29:31 -0300

    arrancanding

--- api/index.js
+++ api/index.js
@@ -23,6 +23,6 @@ const { conn } = require("./src/db.js");
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log("%s TODO OK !! ESCUCHANDO!!");
+    console.log(" TODO OK !! ESCUCHANDO!!");
   });
 });
